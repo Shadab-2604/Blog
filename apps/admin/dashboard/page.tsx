b0006ac2aedab49cdc2d942235f4a1ae62155e97
Shadab-2604 <138616478+Shadab-2604@users.noreply.github.com>
2025-04-07 06:25:39 +0530

    bug fixes

--- apps/admin/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-dynamic"
-
-import { AdminDashboard } from "@/components/admin-dashboard"
-
-export default function DashboardPage() {
-  return <AdminDashboard />
-}
-
--- apps/admin/posts/preview/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-dynamic"
-
-import { PreviewPostContent } from "@/components/preview-post-content"
-
-export default function PreviewPostPage() {
-  return <PreviewPostContent />
-}
-
